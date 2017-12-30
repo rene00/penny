20e0c7b00e983dcb1b960900e3abd1196ffb47d8
Rene Cunningham <rene@compounddata.com>
2017-12-30 10:42:47 +0000

    Update path to transaction hash generator and add py3 support.

--- scripts/find_duplicate_transactions.py
+++ scripts/find_duplicate_transactions.py
@@ -1,22 +1,16 @@
 import sys
 import os
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
 from app import app
 from app.models import session, Transaction, BankAccount
+from app.common import util
 import hashlib
 from flask.ext.script import Manager
 from sqlalchemy.orm.exc import NoResultFound
 
 
-manager = Manager(app)
 
-def get_hash(transaction):
-    _hash = hashlib.md5()
-    for key in [transaction.date, transaction.debit, transaction.credit, transaction.memo]:
-        if key:
-            _hash.update(str(key))
-    return _hash.hexdigest()
+manager = Manager(app)
 
 
 @manager.option('--bankaccount-id', dest='bankaccount_id', default=15)
@@ -35,16 +29,24 @@ def run(bankaccount_id):
     track = {}
 
     for transaction in transactions:
-        _hash = get_hash(transaction)
+        _hash = util.generate_transaction_hash(
+            date=transaction.date,
+            debit=transaction.debit,
+            credit=transaction.credit,
+            memo=transaction.memo,
+            fitid=transaction.fitid,
+            bankaccount_id=transaction.bankaccount.id
+        )
         if _hash not in track:
             track[_hash] = transaction.id
         else:
-            t = session.query(Transaction).filter(Transaction.id == track[_hash]).one()
+            t = (session.query(Transaction).
+                 filter(Transaction.id == track[_hash]).one())
             print("dupe: {0}, {1}".format(t.id, transaction.id))
             print(t)
             print(transaction)
             msg = 'Delete {0}?'.format(transaction.id)
-            shall = raw_input("%s (y/N) " % msg).lower() == 'y'
+            shall = input("%s (y/N) " % msg).lower() == 'y'
             if shall:
                 transaction.is_deleted = True
                 session.commit()
