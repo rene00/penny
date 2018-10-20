5c14d7794003711fdb05f9079665bd719d2bcbf4
Rene Cunningham <rene@compounddata.com>
2018-10-20 22:23:50 +1100

    Update to filetypes.py.

--- app/common/filetypes.py
+++ app/common/filetypes.py
@@ -1,4 +1,5 @@
 import csv
+from io import StringIO
 from ofxparse import OfxParser
 from ofxparse.ofxparse import OfxParserException
 from bs4 import BeautifulSoup
@@ -26,6 +27,29 @@ def is_csv(csv_file):
         raise FileTypesError('missing csv field; {}'.format(missing))
     else:
         return True
+
+
+def is_paypal_csv(csv_file):
+    with open(csv_file, 'r') as fh:
+        transactions = fh.read()
+    data = csv.DictReader(StringIO(transactions))
+    # these 10 fielders are found in a paypal csv file
+    field_names = ['Date', 'Time', 'Time Zone', 'Name', 'Type', 'Status',
+                   'Currency', 'Gross', 'Fee']
+
+    try:
+        header = data.next()
+    except:
+        raise FileTypesError('failed reading file')
+
+    for key, value in header.iteritems():
+        if key.strip() in field_names:
+            field_names.remove(key.strip())
+
+    if not field_names:
+        return True
+    else:
+        return False
 
 
 def is_ofx(ofx_file):
