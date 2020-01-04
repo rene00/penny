e33ffefe27386b6eb0d116d2b8ffe8e51b17337f
Rene Cunningham <rene@compounddata.com>
2020-01-04 11:20:00 +1100

    Update transaction utils to support relative and absolute filepaths.

--- app/common/attachments/__init__.py
+++ app/common/attachments/__init__.py
@@ -3,19 +3,20 @@ from werkzeug import secure_filename
 import hashlib
 
 
-def mkdir_for_transaction(filepath):
-    filedir = os.path.dirname(os.path.realpath(filepath))
+def mkdir_for_transaction(parentdir, relfilepath):
+    filedir = os.path.join(parentdir, os.path.dirname(relfilepath))
     if not os.path.isdir(filedir):
         os.makedirs(filedir)
 
 
-def get_filepath_for_transaction_attachment(app, transaction, filename):
-    filepath = os.path.join(
-        app.config['TRANSACTION_ATTACHMENTS_UPLOAD_FOLDER'],
+def get_filepath_for_transaction_attachment(parentdir, transaction, filename):
+    "Return absolute and relative filepath of transaction attachment."
+    relfilepath = os.path.join(
         str(transaction.id),
-        secure_filename(filename))
-    mkdir_for_transaction(filepath)
-    return filepath
+        secure_filename(filename)
+    )
+    mkdir_for_transaction(parentdir, relfilepath)
+    return (os.path.join(parentdir, relfilepath), os.path.join('/', relfilepath))
 
 
 def get_hash_of_file(filepath):
