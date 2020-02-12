fcec43b3ce3bc3d2a16bca0f7c7ad4f820494454
Rene Cunningham <rene@compounddata.com>
2020-02-12 21:45:07 +1100

    Fix end month in monthly account breakdown report

--- app/resources/reports/controllers.py
+++ app/resources/reports/controllers.py
@@ -10,6 +10,7 @@ from flask_wtf import Form
 from wtforms import DateField, SelectField
 from dateutil.rrule import rrule, MONTHLY
 import datetime
+import calendar
 
 reports = Blueprint('reports', __name__, url_prefix='/reports')
 
@@ -77,12 +78,28 @@ class FormBasicDates(Form):
         return self.datepicker_end.data.strftime(fmt)
 
 
+class MonthlyBreakdownMonth:
+    def __init__(self, year, month, amount):
+        self.year = year
+        self.month = month
+        self.amount = amount
+
+    @property
+    def daycount(self):
+        return calendar.monthrange(int(self.year), int(self.month))[1]
+
+    @property
+    def date(self):
+        return '{0}-{1}'.format(self.year, self.month)
+
+
 def generate_report_account_monthly(account, start_date, end_date):
     report = {'transactions': {}}
     data = {}
 
     transactions = models.db.session.query(
-            func.date_format(models.Transaction.date, '%Y-%m').label("month"),
+            func.date_format(models.Transaction.date, '%Y').label("year"),
+            func.date_format(models.Transaction.date, '%m').label("month"),
             func.sum(models.Transaction.credit).label("credit"),
             func.sum(models.Transaction.debit).label("debit"),
         ) \
@@ -101,12 +118,19 @@ def generate_report_account_monthly(account, start_date, end_date):
         amount = util.convert_to_float(
             int(transaction.credit + transaction.debit)
         )
-        data[transaction.month] = amount
+        month = MonthlyBreakdownMonth(
+            year=transaction.year, month=transaction.month, amount=amount
+        )
+        data[month.date] = month
 
     for d in rrule(freq=MONTHLY, dtstart=start_date, until=end_date):
-        month = d.strftime("%Y-%m")
-        amount = data.get(month, "$0.00")
-        report['transactions'][month] = amount
+        month = MonthlyBreakdownMonth(
+            year=d.strftime("%Y"), month=d.strftime("%m"), amount="$0.00"
+        )
+        exists = data.get(month.date)
+        if exists:
+            month.amount = exists.amount
+        report['transactions'][month.date] = month
 
     return report
 
