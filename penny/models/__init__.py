5e93c51e6e5e72e4d512e8c95b1bd26ccbb89f3a
Rene Cunningham <rene@compounddata.com>
2022-05-14 15:01:26 +1000

    fix

--- penny/models/__init__.py
+++ penny/models/__init__.py
@@ -19,12 +19,14 @@ def utcnow():
     "Return a UTC datetime object with the current time."
     return datetime.utcnow().replace(tzinfo=pytz.utc)
 
+
 class RolesUsers(db.Model):
     __tablename__ = "roles_users"
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
     role_id = db.Column("role_id", db.Integer, db.ForeignKey("role.id"))
+
 
 class Role(db.Model, RoleMixin):
     __tablename__ = "role"
