a98b27178956d87caac077d22bd5f1d535036c89
rene <rene@compounddata.com>
2022-11-11 18:10:33 +1100

    fix: build on master (#145)

--- tests/test_account.py
+++ tests/test_account.py
@@ -1,17 +1,13 @@
-import pytest
 from flask import json
-from datetime import datetime
-from .conftest import (
-    create_entity,
-    create_bankaccount,
-    create_account
-)
+from .conftest import create_entity, create_bankaccount, create_account
+
 
 def test_account(client, auth):
     with client:
-        auth.login()
+        r = auth.login()
+        print(r.data)
         create_entity(client)
-        create_bankaccount(client)
+        r = create_bankaccount(client)
         response = create_account(client)
         assert b'value="test-account-name"' in response.data
         assert b'value="test-account-desc"' in response.data
@@ -19,6 +15,6 @@ def test_account(client, auth):
         response = client.get("/data/accounts/?search=&order=desc&offset=0&limit=1")
         data = json.loads(response.data)
         assert 200 == response.status_code
-        assert data['rows'][0]['name'] == 'test-account-name'
-        assert data['rows'][0]['id'] == 1
-        assert data['rows'][0]['entity_name'] == 'test-entity-name'
+        assert data["rows"][0]["name"] == "test-account-name"
+        assert data["rows"][0]["id"] == 1
+        assert data["rows"][0]["entity_name"] == "test-entity-name"
