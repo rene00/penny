b1a7818932d02a049efc353c7ff5c0f8e7992536
Rene Cunningham <rene@compounddata.com>
2023-02-12 14:38:57 +1100

    more progress on sbadmin2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>penny</h1>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <Sidebar/>
+    </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Sidebar from './components/Sidebar.vue'
+
+export default defineComponent({
+    components: {
+        Sidebar,
+    },
+})
+</script>
+
+
