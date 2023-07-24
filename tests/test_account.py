19bc82cb5518760a8af57fdebf90ef88ca78ae1c
Rene Cunningham <rene@compounddata.com>
2023-07-24 12:09:23 +1000

    fix: clean up test_account

--- tests/test_account.py
+++ tests/test_account.py
@@ -2,19 +2,18 @@ from flask import json
 from .conftest import create_entity, create_bankaccount, create_account
 
 
-def test_account(client, auth):
+def test_account(client, auth) -> None:
     with client:
-        r = auth.login()
-        print(r.data)
+        auth.login()
+
         create_entity(client)
-        r = create_bankaccount(client)
+        create_bankaccount(client)
         response = create_account(client)
         assert b'value="test-account-name"' in response.data
         assert b'value="test-account-desc"' in response.data
 
         response = client.get("/data/accounts/?search=&order=desc&offset=0&limit=1")
         data = json.loads(response.data)
-        assert 200 == response.status_code
         assert data["rows"][0]["name"] == "test-account-name"
         assert data["rows"][0]["id"] == 1
         assert data["rows"][0]["entity_name"] == "test-entity-name"
